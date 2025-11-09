5bd95e42f36843ab3be3ce283c438984fc83ae2e
Dana Bernal <danabernal222@unisangil.edu.co>
2025-11-08 21:19:10 -0500

    Agregar Swagger API

---
+++ drogueria/src/main/java/com/drogueria/drogueria/Documentation/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.drogueria.drogueria.Documentation;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API - BeautyApp")
+                        .version("1.0.0")
+                        .description("Documentación del backend con Swagger UI")
+                        .contact(new Contact()
+                                .name("Dana Valentina Bernal León")
+                                .email("danabernal222@unisangil.edu.co")
+                                .url("https://beauty.com")));
+    }
+}
